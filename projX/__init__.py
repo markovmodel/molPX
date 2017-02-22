8c9a8e641f648d086d4583232e3fc129fed1044e
gph82 <guille.perez@fu-berlin.de>
2017-02-22 19:48:51 +0100

    [init] added version attribute

--- projX/__init__.py
+++ projX/__init__.py
@@ -2,5 +2,24 @@ from __future__ import print_function
 
 __author__ = 'gph82'
 
+# http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
+
+try:
+    _dist = get_distribution('projX')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'projX')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
+
 from . import generate
 from . import visualize
+
+

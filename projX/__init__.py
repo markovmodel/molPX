8cc1a0016ec357401bb9dd601aace0c214f7c960
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-03-03 17:24:08 +0100

    use versioneer in init.py

--- projX/__init__.py
+++ projX/__init__.py
@@ -83,31 +83,11 @@ def _version_check(current, testing=False):
             # TODO add loggers
 
     return threading.Thread(target=_impl if _report_status() else lambda: '')
-
-
-# http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
-from pkg_resources import get_distribution, DistributionNotFound
-import os.path
-try:
-    _dist = get_distribution('projX')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'projX')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 
 # start check in background
 _version_check(__version__).start()
 
-
-
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions

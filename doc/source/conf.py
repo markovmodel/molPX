a72ee9340deb1c69c97d92661641aabf0cf56929
gph82 <guille.perez@fu-berlin.de>
2017-03-06 18:48:45 +0100

    [docs/conf] readthedocs cannot find molpx, debuging statements included

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-
+print(sys.path)
+print(os.listdir(sys.path[0]))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.

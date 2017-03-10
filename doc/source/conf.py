f8920eb10ed07509ec4c445e8bec30f73a5de361
Martin Scherer <m.scherer@fu-berlin.de>
2017-03-10 16:04:31 +0100

    [doc] mock imports

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,13 +19,25 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.')) # brute force
-sys.path.insert(0, os.path.abspath('../')) # brute force
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../../molpx'))
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['mdtraj', 'pyemma', 'matplotlib', 'scipy', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
+sys.path.append(os.path.abspath('../../'))
 print(sys.path)
+
+import molpx
+
+version = molpx.__version__
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.

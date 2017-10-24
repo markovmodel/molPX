2a14a7541173f6a367181fda3a864049814ba38b
gph82 <guille.perezu@fu-berlin.de>
2017-10-24 18:42:05 +0200

    [bumutils] refactored stuff into linkutils

--- molpx/_bmutils.py
+++ molpx/_bmutils.py
@@ -1,9 +1,6 @@
 from __future__ import print_function
 import numpy as _np
 import mdtraj as _md
-from matplotlib.widgets import AxesWidget as _AxesWidget
-from matplotlib.cm import get_cmap as _get_cmap
-from matplotlib.colors import rgb2hex as _rgb2hex, is_color_like as _is_color_like, to_hex as _to_hex
 
 try:
     from sklearn.mixture import GaussianMixture as _GMM
@@ -30,8 +27,6 @@ from pyemma.coordinates.transform import TICA as _TICA, PCA as _PCA
 # From coor.util
 from pyemma.util.discrete_trajectories import index_states as _index_states
 from pyemma.util.types import is_string as _is_string,  is_int as _is_int
-
-from scipy.spatial import cKDTree as _cKDTree
 
 def listify_if_int(inp):
     if _is_int(inp):
@@ -618,188 +613,6 @@ def minimize_rmsd2ref_in_sample(sample, ref):
 
     return out_geoms.superpose(ref)
 
-def link_ax_w_pos_2_nglwidget(ax, pos, nglwidget,
-                              crosshairs=True,
-                              dot_color='red',
-                              band_width=None,
-                              radius=False,
-                              directionality=None,
-                              exclude_coord=None,
-                              color_list=None,
-                              list_of_repr_dicts = None
-                              ):
-    r"""
-    Initial idea for this function comes from @arose, the rest is @gph82
-
-    Parameters
-    ---------
-    band_with : None or float,
-        band_width is in units of the axis of (it will be tranlated to pts internally)
-
-    crosshairs : Boolean or str
-        If True, a crosshair will show where the mouse-click ocurred. If 'h' or 'v', only the horizontal or
-        vertical line of the crosshair will be shown, respectively. If False, no crosshair will appear
-
-    dot_color : Anything that yields matplotlib.colors.is_color_like(dot_color)==True
-        Default is 'red'. dot_color='None' yields no dot
-
-    directionality : str or None, default is None
-        If not None, directionality can be either 'a2w' or 'w2a', meaning that connectivity
-         between axis and widget will be only established as
-         * 'a2w' : action in axis   triggers action in widget, but not the other way around
-         * 'w2a' : action in widget triggers action in axis, but not the other way around
-
-    exclude_coord : None or int , default is None
-        The excluded coordinate will not be considered when computing the nearest-point-to-click.
-        Typical use case is for visualize.traj to only compute distances horizontally along the time axis
-
-    color_list : None or list of len(pos)
-        The colors with which the sticky frames will be plotted.
-        Can by anything that yields matplotlib.colors.is_color_like == True
-
-    list_of_repr_dicts : None or list of dictionaries having at least keys 'repr_type' and 'selection' keys.
-        Other **kwargs are currently ignored but will be implemented in the future (see nglview.add_representation
-        for more info). Only active for sticky widgets
-    #TODO consider implementing list_of_repr_dicts in visualize.sample
-    """
-
-    assert directionality in [None, 'a2w', 'w2a'], "The directionality parameter has to be in [None, 'a2w', 'w2a'] " \
-                                                   "not %s"%directionality
-
-    assert crosshairs in [True, False, 'h', 'v'], "The crosshairs parameter has to be in [True, False, 'h','v'], " \
-                                                   "not %s" % crosshairs
-    ipos = _np.copy(pos)
-    if _is_int(exclude_coord):
-        ipos[:,exclude_coord] = 0
-    kdtree = _cKDTree(ipos)
-    assert nglwidget.trajectory_0.n_frames == pos.shape[0], \
-        ("Mismatching frame numbers %u vs %u"%( nglwidget.trajectory_0.n_frames, pos.shape[0]))
-
-    x, y = pos.T
-
-    sticky = False
-    # Are we in a sticky situation?
-    if hasattr(nglwidget, '_hidden_sticky_frames'):
-        cmap = _get_cmap('rainbow')
-        cmap_table = _np.linspace(0, 1, len(x))
-        sticky_overlays_by_frame = transpose_geom_list(nglwidget._hidden_sticky_frames)
-        overlay_iterator_by_frame = {ff: iter(sgeom) for ff, sgeom in enumerate(sticky_overlays_by_frame)}
-        # TODO: create a path through the colors that maximizes distance between averages (otherwise some colors
-        # are too close
-        if color_list is None:
-            sticky_colors_hex = [_rgb2hex(cmap(ii)) for ii in _np.random.permutation(cmap_table)]
-        elif isinstance(color_list, list) and len(color_list)==len(pos):
-            sticky_colors_hex = [_to_hex(cc) for cc in color_list]
-        else:
-            raise TypeError('argument color_list should be either None or a list of len(pos), '
-                            'instead of type %s and len %u'%(type(color_list), len(color_list)))
-        sticky_rep = 'cartoon'
-        if nglwidget._hidden_sticky_frames[0].top.n_residues < 10:
-            sticky_rep = 'ball+stick'
-        if list_of_repr_dicts is None:
-            list_of_repr_dicts = [{'repr_type': sticky_rep, 'selection': 'all'}]
-        sticky = True
-
-    # Basic interactive objects
-    showclick_objs = []
-    if crosshairs in [True, 'h']:
-        lineh = ax.axhline(ax.get_ybound()[0], c="black", ls='--')
-        setattr(lineh, 'whatisthis', 'lineh')
-        showclick_objs.append(lineh)
-    if crosshairs in [True, 'v']:
-        linev = ax.axvline(ax.get_xbound()[0], c="black", ls='--')
-        setattr(linev, 'whatisthis', 'linev')
-        showclick_objs.append(linev)
-
-    if _is_color_like(dot_color):
-        pass
-    else:
-        raise TypeError('dot_color should be a matplotlib color')
-
-    dot = ax.plot(pos[0,0],pos[0,1], 'o', c=dot_color, ms=7, zorder=100)[0]
-    setattr(dot,'whatisthis','dot')
-    closest_to_click_obj = [dot]
-
-    # Other objects, related to smoothing options
-    if band_width is not None:
-        if radius:
-            band_width_in_pts = int(_np.round(pts_per_axis_unit(ax).mean() * band_width.mean()))
-            rad = ax.plot(pos[0, 0], pos[0, 1], 'o',
-                          ms=_np.round(band_width_in_pts),
-                          c='green', alpha=.25, markeredgecolor='None')[0]
-            setattr(rad, 'whatisthis', 'dot')
-            if not sticky:
-                closest_to_click_obj.append(rad)
-        else:
-            # print("Band_width(x,y) is %s" % (band_width))
-            coord_idx = get_ascending_coord_idx(pos)
-            band_width_in_pts = int(_np.round(pts_per_axis_unit(ax)[coord_idx] * band_width[coord_idx]))
-            # print("Band_width in %s is %s pts"%('xy'[coord_idx], band_width_in_pts))
-
-
-            band_call = [ax.axvline, ax.axhline][coord_idx]
-            band_init = [ax.get_xbound, ax.get_ybound][coord_idx]
-            band_type = ['linev',  'lineh'][coord_idx]
-            band = band_call(band_init()[0],
-                             lw=band_width_in_pts,
-                             c="green", ls='-',
-                             alpha=.25)
-            setattr(band, 'whatisthis', band_type)
-            closest_to_click_obj.append(band)
-
-    nglwidget.isClick = False
-    def onclick(event):
-        if crosshairs:
-            for iline in showclick_objs:
-                update2Dlines(iline,event.xdata, event.ydata)
-
-        data = [event.xdata, event.ydata]
-        _, index = kdtree.query(x=data, k=1)
-        for idot in closest_to_click_obj:
-            update2Dlines(idot,x[index],y[index])
-
-        nglwidget.isClick = True
-        if not sticky:
-            nglwidget.frame = index
-        else:
-            nglwidget.add_trajectory(next(overlay_iterator_by_frame[index]))
-            nglwidget.clear_representations(component=nglwidget.n_components)
-            for irepr in list_of_repr_dicts:
-                nglwidget.add_representation(irepr['repr_type'],
-                                             selection=irepr['selection'],
-                                             component=nglwidget.n_components,
-                                             color=sticky_colors_hex[index],
-                                             )
-            ax.plot(pos[index, 0], pos[index, 1], 'o', c=sticky_colors_hex[index], ms=7)
-
-    def my_observer(change):
-        r"""Here comes the code that you want to execute
-        """
-        #for c in change:
-        #    print("%s -> %s" % (c, change[c]))
-        nglwidget.isClick = False
-        _idx = change["new"]
-        try:
-            for idot in closest_to_click_obj:
-                update2Dlines(idot, x[_idx], y[_idx])
-            #print("caught index error with index %s (new=%s, old=%s)" % (_idx, change["new"], change["old"]))
-        except IndexError as e:
-            for idot in closest_to_click_obj:
-                update2Dlines(idot, x[0], y[0])
-            print("caught index error with index %s (new=%s, old=%s)" % (_idx, change["new"], change["old"]))
-            pass
-        #print("set xy = (%s, %s)" % (x[_idx], y[_idx]))
-
-    # Connect axes to widget
-    axes_widget = _AxesWidget(ax)
-    if directionality in [None, 'a2w']:
-        axes_widget.connect_event('button_release_event', onclick)
-
-    # Connect widget to axes
-    if directionality in [None, 'w2a']:
-        nglwidget.observe(my_observer, "frame", "change")
-
-    return axes_widget
 
 def get_ascending_coord_idx(pos, fail_if_empty=False, fail_if_more_than_one=False):
     r"""
@@ -840,68 +653,6 @@ def get_ascending_coord_idx(pos, fail_if_empty=False, fail_if_more_than_one=Fals
             idxs = idxs[0]
     return idxs
 
-def pts_per_axis_unit(mplax, pt_per_inch=72):
-    r"""
-    Return how many pt per axis unit of a given maptplotlib axis a figure has
-
-    Parameters
-    ----------
-
-    mplax : :obj:`matplotlib.axes._subplots.AxesSubplot`
-
-    pt_per_inch : how many points are in an inch (this number should not change)
-
-    Returns
-    --------
-
-    pt_per_xunit, pt_per_yunit
-
-    """
-
-    # matplotlib voodoo
-    # Get bounding box
-    bbox = mplax.get_window_extent().transformed(mplax.get_figure().dpi_scale_trans.inverted())
-
-    span_inch = _np.array([bbox.width, bbox.height], ndmin=2).T
-
-    span_units = [mplax.get_xlim(), mplax.get_ylim()]
-    span_units = _np.diff(span_units, axis=1)
-
-    inch_per_unit = span_inch / span_units
-    return inch_per_unit * pt_per_inch
-
-def update2Dlines(iline, x, y):
-    """
-    provide a common interface to update objects on the plot to a new position (x,y) depending
-    on whether they are hlines, vlines, dots etc
-
-    Parameters
-    ----------
-
-    iline: :obj:`matplotlib.lines.Line2D` object
-
-    x : float with new position
-
-    y : float with new position
-
-    tested:False
-    """
-    # TODO FIND OUT A CLEANER WAY TO DO THIS (dict or class)
-
-    if not hasattr(iline,'whatisthis'):
-        raise AttributeError("This method will only work if iline has the attribute 'whatsthis'")
-    else:
-        # TODO find cleaner way of distinguishing these 2Dlines
-        if iline.whatisthis in ['dot']:
-            iline.set_xdata((x))
-            iline.set_ydata((y))
-        elif iline.whatisthis in ['lineh']:
-            iline.set_ydata((y,y))
-        elif iline.whatisthis in ['linev']:
-            iline.set_xdata((x,x))
-        else:
-            # TODO: FIND OUT WNY EXCEPTIONS ARE NOT BEING RAISED
-            raise TypeError("what is this type of 2Dline?")
 
 def running_avg_idxs(l, n, symmetric=True, debug=False):
     r"""
